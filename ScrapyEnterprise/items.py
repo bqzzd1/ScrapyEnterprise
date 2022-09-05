f8561d31c7237aa775bebd2bd5525665df4cbf16
bqzzd <1690670808@qq.com>
2022-09-05 14:08:27 +0800

    update

--- ScrapyEnterprise/items.py
+++ ScrapyEnterprise/items.py
@@ -1,5 +1,4 @@
 # Define here the models for your scraped items
-#
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/items.html
 
@@ -7,6 +6,5 @@ import scrapy
 
 
 class ScrapyenterpriseItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
+
     pass
